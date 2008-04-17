43cc15b6d1dea89d60e25e0abbcd13a1424ccef3
Heiko Klein <heikok@met.no>
2008-04-17 05:55:40 +0000

    writing output to local directory, readable for testNetCDFReadWrite.sh

--- test/testFeltReadNetCDFWrite.sh
+++ test/testFeltReadNetCDFWrite.sh
@@ -1,17 +1,15 @@
 #! /bin/sh
 echo "testing conversion NetCDF to NetCDF"
-file=/tmp/testfile.nc
+file=../../test/test.nc
 cd ../src/binSrc
 ./utplukk -c utplukk.cfg --output.file=${file}
 if [ $? != 0 ]; then
   echo "failed converting felt to nc"
-  rm -f ${file}
   exit 1
 fi
 if [ ! -f ${file} ]; then
   echo "failed find nc-outputfile"
   exit 1
 fi
-rm -f ${file}
 echo "success"
 exit 0
