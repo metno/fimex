a0933698effced4818757a53ca506a7cd34a3c09
Heiko Klein <heikok@met.no>
2009-11-30 15:32:47 +0000

    setting debian-packages manually (requires otherwise reading of rules)

--- m4/debian.mk
+++ m4/debian.mk
@@ -16,29 +16,22 @@ DEBIAN_DIR = $(PKG_DIR)/debian
 DEBIAN_PACKAGE = $(PACKAGE)_$(VERSION)
 ARCH = `dpkg-architecture -qDEB_HOST_ARCH_CPU`
 DEBIAN_PACKAGE_NAME = `head -n1 $(top_srcdir)/debian_files/changelog | sed "s/ (/_/" | sed "s/).*//"`_$(ARCH).deb
-
+DEBIAN_PACKAGE_VERSION = `head -n1 $(top_srcdir)/debian_files/changelog | sed "s/.*(\(.*\)).*/\1/"`
 prepare-debian: dist clean-debian
 	tar xvzf $(PKG_DIR).tar.gz
 	mkdir -p $(DEBIAN_DIR)
 	cp $(PKG_DIR).tar.gz $(DEBIAN_PACKAGE).orig.tar.gz
-
-
-#debian:	dist clean-debian 
-#	tar xvzf $(PKG_DIR).tar.gz
-#	mkdir -p $(DEBIAN_DIR)
-#	cp $(PKG_DIR).tar.gz $(DEBIAN_PACKAGE).orig.tar.gz
-#	rm -rf $(DEBIAN_DIR)/*
-#	cp -r $(top_srcdir)/debian_files/* $(DEBIAN_DIR)
-#	chmod 774 $(DEBIAN_DIR)/rules
-#	cd $(PKG_DIR) && dpkg-buildpackage -rfakeroot -us -uc
-#	lintian $(DEBIAN_PACKAGE_NAME)
 
 update-debian:
 	rm -rf $(DEBIAN_DIR)/*
 	cp -r $(top_srcdir)/debian_files/* $(DEBIAN_DIR)
 	chmod 774 $(DEBIAN_DIR)/rules
 	cd $(PKG_DIR) && dpkg-buildpackage -rfakeroot -us -uc -nc
-	lintian $(DEBIAN_PACKAGE_NAME)
+	lintian libfelt_$(DEBIAN_PACKAGE_VERSION)_$(ARCH).deb
+	lintian libfelt-dev_$(DEBIAN_PACKAGE_VERSION)_$(ARCH).deb
+	lintian libfimex_$(DEBIAN_PACKAGE_VERSION)_$(ARCH).deb
+	lintian libfimex-dev_$(DEBIAN_PACKAGE_VERSION)_$(ARCH).deb
+	lintian fimex-bin_$(DEBIAN_PACKAGE_VERSION)_$(ARCH).deb
 
 debian: prepare-debian update-debian
 
