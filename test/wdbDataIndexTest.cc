5b47b2d9a816cc35df29f7c477d67367dc85d2c6
Vegard Bønes <vegardb@met.no>
2011-03-18 17:20:33 +0000

    Corrected a test case (which now fails)

--- test/wdbDataIndexTest.cc
+++ test/wdbDataIndexTest.cc
@@ -55,6 +55,11 @@ public:
 	TestingGridData(const wdb::Level & lvl, const std::string & time = "2011-03-18 06:00:00") :
 		wdb::GridData(wdb::Parameter("air temperature", "C"), lvl, 0, t(time), 0)
 	{}
+
+	TestingGridData(const wdb::Parameter & parameter, const std::string & time = "2011-03-18 06:00:00") :
+		wdb::GridData(parameter, wdb::Level("distance above ground", 0, 0), 0, t(time), 0)
+	{}
+
 };
 
 struct same_entity
@@ -149,6 +154,26 @@ BOOST_AUTO_TEST_CASE(setsCorrectTimeDimensionSize)
 	BOOST_CHECK_EQUAL(2, timeDimension->getLength());
 }
 
+BOOST_AUTO_TEST_CASE(picksUpAllTimesFromSameParameter)
+{
+	// All times for varying levels should be picked up
+
+	std::vector<wdb::GridData> gridData;
+	gridData.push_back(TestingGridData(wdb::Level("lvl", 2, 2), "2010-03-18 06:00:00"));
+	gridData.push_back(TestingGridData(wdb::Level("lvl", 2, 2), "2010-03-18 07:00:00"));
+	gridData.push_back(TestingGridData(wdb::Level("lvl", 0, 0), "2000-01-01 00:00:00"));
+
+	const wdb::DataIndex di(gridData);
+	CDM cdm;
+	di.populate(cdm);
+
+	const CDM::DimVec & dims = cdm.getDimensions();
+
+	CDM::DimVec::const_iterator timeDimension = std::find_if(dims.begin(), dims.end(), same_entity("time"));
+	BOOST_REQUIRE(timeDimension != dims.end());
+	BOOST_CHECK_EQUAL(3, timeDimension->getLength());
+}
+
 BOOST_AUTO_TEST_CASE(ignoresIrrelevantTimeDimensions)
 {
 	// Any parameters which only have one time step will not get a time dimension
@@ -157,10 +182,7 @@ BOOST_AUTO_TEST_CASE(ignoresIrrelevantTimeDimensions)
 	std::vector<wdb::GridData> gridData;
 	gridData.push_back(TestingGridData(wdb::Level("lvl", 2, 2), "2010-03-18 06:00:00"));
 	gridData.push_back(TestingGridData(wdb::Level("lvl", 2, 2), "2010-03-18 07:00:00"));
-
-	// lvl 0 has only one dimension for time.
-	// it should therefore be ignored
-	gridData.push_back(TestingGridData(wdb::Level("lvl", 0, 0), "2000-01-01 00:00:00"));
+	gridData.push_back(TestingGridData(wdb::Parameter("timeless", "stuff"), "2000-01-01 00:00:00"));
 
 	const wdb::DataIndex di(gridData);
 	CDM cdm;
@@ -172,7 +194,6 @@ BOOST_AUTO_TEST_CASE(ignoresIrrelevantTimeDimensions)
 	BOOST_REQUIRE(timeDimension != dims.end());
 	BOOST_CHECK_EQUAL(2, timeDimension->getLength());
 }
-
 
 
 
