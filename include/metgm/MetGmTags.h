fd05192e55c18674c620549cc41a45504df53b59
Aleksandar Babic <aleksandarb@met.no>
2011-07-04 07:16:17 +0000

    this class binds other tags for given CDM variable

---
+++ include/metgm/MetGmTags.h
@@ -0,0 +1,53 @@
+/*
+ * Fimex
+ *
+ * (C) Copyright 2011, met.no
+ *
+ * Project Info:  https://wiki.met.no/fimex/start
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+
+/**
+  * Used as private/implementation class
+  */
+
+#ifndef METGM_TAGS_H
+#define METGM_TAGS_H
+
+// METGM C lib
+//
+#include "metgm.h"
+
+// implementation classes
+//
+#include "MetGmVerticalTag.h"
+#include "MetGmHorizontalTag.h"
+
+// boost
+#include <boost/shared_ptr.hpp>
+
+
+namespace MetNoFimex {
+
+    class MetGmTags {
+    public:
+        boost::shared_ptr<MetGmVerticalTag>   vTag;
+        boost::shared_ptr<MetGmHorizontalTag> hTag;
+    };
+}
+
+#endif // METGM_TAGS_H
