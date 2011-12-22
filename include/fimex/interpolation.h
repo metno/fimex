c6f52ae6bc8a4ce9113d35a4b7104f2a52fbd583
Heiko Klein <heikok@met.no>
2011-12-22 08:38:15 +0000

    using 'static inline' for backwards compatibility

--- include/fimex/interpolation.h
+++ include/fimex/interpolation.h
@@ -285,7 +285,7 @@ extern int mifi_points2position(double* points, const int n, const double* axis,
  *
  *  @return the position of x, y, z
  */
-inline int mifi_3d_array_position(int x, int y, int z, int ix, int iy, int iz) {
+static inline int mifi_3d_array_position(int x, int y, int z, int ix, int iy, int iz) {
     return (z*iy + y)*ix + x;
 }
 
