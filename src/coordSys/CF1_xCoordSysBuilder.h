f9938ad90184e09e95a56d667f624f9555da71f7
Heiko Klein <heikok@met.no>
2012-03-28 18:03:04 +0000

    refactoring listCoordinateSystems to be easier extendible (for WRFConvention)

---
+++ src/coordSys/CF1_xCoordSysBuilder.h
@@ -0,0 +1,52 @@
+/*
+ * Fimex, CF1xCoordSysBuilder.h
+ *
+ * (C) Copyright 2012, met.no
+ *
+ * Project Info:  https://wiki.met.no/fimex/start
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ *  Created on: Mar 28, 2012
+ *      Author: Heiko Klein
+ */
+
+#ifndef CF1XCOORDSYSBUILDER_H_
+#define CF1XCOORDSYSBUILDER_H_
+
+#include "coordSys/CoordSysBuilder.h"
+
+namespace MetNoFimex
+{
+
+/**
+ * coordinate system builder to handle CF-1.x and compatible coordinate systems (COARDS)
+ */
+class CF1_xCoordSysBuilder : public CoordSysBuilder
+{
+public:
+    CF1_xCoordSysBuilder();
+    virtual ~CF1_xCoordSysBuilder();
+    virtual void setCDM(boost::shared_ptr<CDM> cdm);
+    virtual std::string getName() {return "CF-1.x";}
+    virtual bool isMine();
+    virtual std::vector<boost::shared_ptr<const CoordinateSystem> > listCoordinateSystems();
+private:
+    boost::shared_ptr<CDM> cdm_;
+};
+
+} /* namespace MetNoFimex */
+#endif /* CF1XCOORDSYSBUILDER_H_ */
