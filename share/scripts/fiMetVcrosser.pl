aed59d6814c5ab2857ca2e6c17f20fd8f6ae92f0
Heiko Klein <heikok@met.no>
2015-11-24 14:15:34 +0000

    correcting license

--- share/scripts/fiMetVcrosser.pl
+++ share/scripts/fiMetVcrosser.pl
@@ -8,20 +8,20 @@ use Encode qw();
 
 Copyright (C) 2013 met.no
 
-This file is part of METAMOD
+This file is part of Fimex
 
-METAMOD is free software; you can redistribute it and/or modify
+Fimex is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
-METAMOD is distributed in the hope that it will be useful,
+Fimex is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with METAMOD; if not, write to the Free Software
+along with Fimex; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 =end LICENSE
