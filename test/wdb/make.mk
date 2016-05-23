147722eacd90f1d39005e404e7f2267abbb01d49
Alexander Bürger <alexander.buerger@met.no>
2016-05-23 08:00:08 +0200

    wdb test was missing files in EXTRA_DIST

--- test/wdb/make.mk
+++ test/wdb/make.mk
@@ -1,23 +1,24 @@
 if HAVE_LIBPQ
- 
-  CC_TESTS += wdbTest
-  
-  wdbTest_SOURCES = \
-  	wdb/TestingGridData.h wdb/TestingGridData.cpp \
-  	wdb/GridDataFactory.h wdb/GridDataFactory.cpp \
-  	wdb/Wdb2CdmBuilderTest.cc \
-  	wdb/WdbIndexTest.cc \
-  	wdb/ConfigurationTest.cpp \
-  	wdb/GlobalWdbConfigurationTest.cpp \
-  	wdb/TimeHandlerTest.cpp
-  	
-  wdbTest_CPPFLAGS = $(AM_CPPFLAGS) -I$(top_srcdir)/src/ -DTEST_DIR=\"@srcdir@/wdb\" @MIFI_PQ_CPPFLAGS@
-  
-  wdbTest_LDFLAGS = $(AM_LDFLAGS) ../src/libfimex.la
-  
-  LDADD += @BOOST_PROGRAM_OPTIONS_LIB@
-  
+
+CC_TESTS += wdbTest
+
+wdbTest_SOURCES = \
+	wdb/TestingGridData.h wdb/TestingGridData.cpp \
+	wdb/GridDataFactory.h wdb/GridDataFactory.cpp \
+	wdb/Wdb2CdmBuilderTest.cc \
+	wdb/WdbIndexTest.cc \
+	wdb/ConfigurationTest.cpp \
+	wdb/GlobalWdbConfigurationTest.cpp \
+	wdb/TimeHandlerTest.cpp
+
+wdbTest_CPPFLAGS = $(AM_CPPFLAGS) -I$(top_srcdir)/src/ -DTEST_DIR=\"@srcdir@/wdb\" @MIFI_PQ_CPPFLAGS@
+wdbTest_LDFLAGS = $(AM_LDFLAGS) ../src/libfimex.la
+
+LDADD += @BOOST_PROGRAM_OPTIONS_LIB@
+
 test_wdb: wdbTest
 	@./wdbTest
 
 endif
+
+EXTRA_DIST += wdb/local.wdb.xml wdb/wdb_config.xml wdb/wdb.wdbml
