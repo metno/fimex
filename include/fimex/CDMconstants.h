d29e0e4a7bd54c7e6482fcba73d7adea2724bd9b
Alexander Bürger <alexander.buerger@met.no>
2016-06-14 08:00:08 +0200

    Version 0.63.1

--- include/fimex/CDMconstants.h
+++ include/fimex/CDMconstants.h
@@ -24,9 +24,9 @@
  */
 #define MIFI_VERSION_MAJOR  0
 #define MIFI_VERSION_MINOR 63
-#define MIFI_VERSION_PATCH  0
+#define MIFI_VERSION_PATCH  1
 #define MIFI_VERSION_STATUS 0xF0
-#define MIFI_VERSION_STRING "0.63"
+#define MIFI_VERSION_STRING "0.63.1"
 
 #define MIFI_VERSION_INT(major,minor,patch) \
     (1000000*major + 1000*minor + patch)
