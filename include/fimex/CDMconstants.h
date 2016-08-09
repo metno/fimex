32cb2a5008ad13972ec28b967234dcf44b3fb953
Alexander Bürger <alexander.buerger@met.no>
2016-08-09 08:00:08 +0200

    continue with 0.64beta after rolling out 0.63

--- include/fimex/CDMconstants.h
+++ include/fimex/CDMconstants.h
@@ -23,10 +23,10 @@
  * touch configure.ac after changing these numbers
  */
 #define MIFI_VERSION_MAJOR  0
-#define MIFI_VERSION_MINOR 63
-#define MIFI_VERSION_PATCH  1
-#define MIFI_VERSION_STATUS 0xF0
-#define MIFI_VERSION_STRING "0.63.1"
+#define MIFI_VERSION_MINOR 64
+#define MIFI_VERSION_PATCH  0
+#define MIFI_VERSION_STATUS 0xB0
+#define MIFI_VERSION_STRING "0.64beta"
 
 #define MIFI_VERSION_INT(major,minor,patch) \
     (1000000*major + 1000*minor + patch)
