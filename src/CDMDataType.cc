e0947c16f652076f58f2da9977c1f266aa17533f
Alexander Bürger <alexander.buerger@met.no>
2016-11-30 08:00:08 +0100

    support CDM_STRINGS in datatype2string

--- src/CDMDataType.cc
+++ src/CDMDataType.cc
@@ -27,40 +27,58 @@
 namespace MetNoFimex
 {
 
-CDMDataType string2datatype(const std::string& s) {
-	std::string str(string2lowerCase(s));
-	if (str == "double") { return CDM_DOUBLE; }
-	else if (str == "float") { return CDM_FLOAT; }
-	else if (str == "int") { return CDM_INT; }
-    else if (str == "long") { return CDM_INT; }
-	else if (str == "short") { return CDM_SHORT; }
-	else if (str == "char") { return CDM_CHAR; }
-    else if (str == "int64") { return CDM_INT64; }
-    else if (str == "uint64") { return CDM_UINT64; }
-    else if (str == "uint") { return CDM_UINT; }
-    else if (str == "ushort") { return CDM_USHORT; }
-    else if (str == "uchar") { return CDM_UCHAR; }
-	else if (str == "string") { return CDM_STRING; }
-    else if (str == "String") { return CDM_STRING; }
-	else { return CDM_NAT; }
+namespace {
+const std::string S_DOUBLE  = "double";
+const std::string S_FLOAT   = "float";
+const std::string S_INT = "int", S_LONG = "long";
+const std::string S_SHORT   = "short";
+const std::string S_CHAR    = "char";
+const std::string S_STRING  = "string";
+const std::string S_UCHAR   = "uchar";
+const std::string S_USHORT  = "ushort";
+const std::string S_UINT    = "uint";
+const std::string S_INT64   = "int64";
+const std::string S_UINT64  = "uint64";
+const std::string S_STRINGS = "strings";
+const std::string S_NAT     = "NAT"; // not a type
+} // namespace
+
+CDMDataType string2datatype(const std::string& s)
+{
+    std::string str(string2lowerCase(s));
+    if      (str == S_DOUBLE)  { return CDM_DOUBLE; }
+    else if (str == S_FLOAT)   { return CDM_FLOAT; }
+    else if (str == S_INT || str == S_LONG)
+                               { return CDM_INT; }
+    else if (str == S_SHORT)   { return CDM_SHORT; }
+    else if (str == S_CHAR)    { return CDM_CHAR; }
+    else if (str == S_INT64)   { return CDM_INT64; }
+    else if (str == S_UINT64)  { return CDM_UINT64; }
+    else if (str == S_UINT)    { return CDM_UINT; }
+    else if (str == S_USHORT)  { return CDM_USHORT; }
+    else if (str == S_UCHAR)   { return CDM_UCHAR; }
+    else if (str == S_STRING)  { return CDM_STRING; }
+    else if (str == S_STRINGS) { return CDM_STRINGS; }
+    else { return CDM_NAT; }
 }
 
-std::string datatype2string(CDMDataType type) {
-	switch(type) {
-	   case CDM_DOUBLE: return "double";
-	   case CDM_FLOAT: return "float";
-	   case CDM_INT: return "int";
-	   case CDM_SHORT: return "short";
-	   case CDM_CHAR: return "char";
-	   case CDM_STRING: return "String";
-       case CDM_UCHAR: return "uchar";
-       case CDM_USHORT: return  "ushort";
-	   case CDM_UINT: return  "uint";
-       case CDM_INT64: return  "int64";
-       case CDM_UINT64: return  "uint64";
-	   default: return "NAT"; // not a type
-	}
+std::string datatype2string(CDMDataType type)
+{
+    switch (type) {
+    case CDM_DOUBLE:  return S_DOUBLE;
+    case CDM_FLOAT:   return S_FLOAT;
+    case CDM_INT:     return S_INT;
+    case CDM_SHORT:   return S_SHORT;
+    case CDM_CHAR:    return S_CHAR;
+    case CDM_UCHAR:   return S_UCHAR;
+    case CDM_USHORT:  return S_USHORT;
+    case CDM_UINT:    return S_UINT;
+    case CDM_INT64:   return S_INT64;
+    case CDM_UINT64:  return S_UINT64;
+    case CDM_STRING:  return S_STRING;
+    case CDM_STRINGS: return S_STRINGS;
+    case CDM_NAT:     return S_NAT; // not a type
+    }
 }
-
 
 }
