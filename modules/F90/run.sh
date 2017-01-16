8d435e081fcbf5024c4316a13e740a33541edb77
Heiko Klein <heiko.klein@met.no>
2017-01-16 16:52:34 +0100

    using Fortran2003 argument counter instead of gfortran buildin

--- modules/F90/run.sh
+++ modules/F90/run.sh
@@ -3,6 +3,6 @@ FIMEXDIR=/disk1/fimexTrunk/
 set -x
 ncgen -b testOut.cdl
 gfortran -g -c fimex.f90 && \
-gcc -g -c _gfortran_iargc.c && \
-gfortran -g -o fortran_test fortran_test.f90 fimex.o _gfortran_iargc.o -L$FIMEXDIR/lib -lfimex -Wl,-rpath,$FIMEXDIR/lib && \
+#gcc -g -c _gfortran_iargc.c && \
+gfortran -g -o fortran_test fortran_test.f90 fimex.o -L$FIMEXDIR/lib -lfimex -Wl,-rpath,$FIMEXDIR/lib && \
 ./fortran_test /opdata/arome_norway25/AROME_Norway25_00.nc netcdf surface_air_pressure hPa
