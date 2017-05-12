6a73ed13583d8e4dcd12cde43abab0f1dcb2a468
Martin Raspaud <martin.raspaud@smhi.se>
2017-05-12 10:02:59 +0200

    Add support for oblique mercator projection

---
+++ include/fimex/coordSys/ObliqueMercatorProjection.h
@@ -0,0 +1,52 @@
+/*
+ * Fimex, ObliqueMercatorProjection.h
+ *
+ * (C) Copyright 2017, SMHI
+ *
+ * Project Info:  https://wiki.met.no/fimex/start
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ *  Created on: May 12, 2017
+ *      Author: Martin Raspaud
+ */
+
+#ifndef OBLIQUEMERCATORPROJECTION_H_
+#define OBLIQUEMERCATORPROJECTION_H_
+
+#include "fimex/coordSys/ProjectionImpl.h"
+
+namespace MetNoFimex
+{
+
+/**
+ * @headerfile fimex/coordSys/ObliqueMercatorProjection.h
+ */
+class ObliqueMercatorProjection: public MetNoFimex::ProjectionImpl
+{
+public:
+    ObliqueMercatorProjection();
+    virtual ~ObliqueMercatorProjection();
+    static bool acceptsProj4(const std::string& proj4Str);
+    static std::vector<CDMAttribute> parametersFromProj4(const std::string& proj4);
+protected:
+    virtual std::ostream& getProj4ProjectionPart(std::ostream& oproj) const;
+
+};
+
+}
+
+#endif /* OBLIQUEMERCATORPROJECTION_H_ */
