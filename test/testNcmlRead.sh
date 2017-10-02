bb3dcf7e3d99d6bece41f3495074e474579bb81b
Alexander Bürger <alexander.buerger@met.no>
2017-10-02 08:00:08 +0200

    bugfix testNcmlRead.sh

--- test/testNcmlRead.sh
+++ test/testNcmlRead.sh
@@ -1,24 +1,32 @@
 #! /bin/sh
-if [ ! -f coordTest.nc ]; then
-	exit 0;
-fi
 echo "test reading with ncml"
-if [ ! -f test.ncml ]; then
-   echo "no input data: test.ncml, skipping test..."
-   exit 0;
+
+TEST_SRCDIR=`dirname $0`
+TEST_BINDIR=`pwd`
+cd $TEST_SRCDIR
+
+if [ ! -f coordTest.nc ]; then
+   echo "no input data: coordTest.nc ..."
+   exit 1;
 fi
-./fimex.sh --input.file=test.ncml --output.file=x --output.type=null
+if [ ! -f test.ncml ]; then
+   echo "no input data: test.ncml ..."
+   exit 1;
+fi
+
+"${TEST_BINDIR}/fimex.sh" --input.file=test.ncml --output.file=x --output.type=null
 if [ $? != 0 ]; then
   echo "failed reading ncml"
   exit 1
 fi
-./fimex.sh --input.file=coordTest.nc --input.config=test.ncml --output.file=x --output.type=null
+
+"${TEST_BINDIR}/fimex.sh" --input.file=coordTest.nc --input.config=test.ncml --output.file=x --output.type=null
 if [ $? != 0 ]; then
   echo "failed reading nc with ncml-config"
   exit 1
 fi
 
-./fimex.sh --input.file="glob:data/joinExistingAgg*.nc" --output.file=x --output.type=null
+"${TEST_BINDIR}/fimex.sh" --input.file="glob:data/joinExistingAgg*.nc" --output.file=x --output.type=null
 if [ $? != 0 ]; then
   echo "failed reading glob:data/joinExistingAgg*.nc"
   exit 1
