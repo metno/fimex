e96acec079708ad39591924497b3e37c226a5eb9
Alexander Bürger <alexander.buerger@met.no>
2019-01-03 08:00:08 +0100

    bugfix testFiGrbmlCat.sh
     * do not delete file from other test, testFiIndexGribs.sh

--- test/testFiGrbmlCat.sh
+++ test/testFiGrbmlCat.sh
@@ -9,23 +9,24 @@ if [ ! -f test.grb1 ]; then
    exit 0
 fi
 
-rm -f test.grb1.grbml
+cp test.grb1 cat.grb1
+rm -f cat.grb1.grbml
 
-./fiIndexGribs.sh -i test.grb1
-if [ $? != 0 -o ! -f test.grb1.grbml ]; then
-  echo "failed writing test.grb1.grbml"
+./fiIndexGribs.sh -i cat.grb1
+if [ $? != 0 -o ! -f cat.grb1.grbml ]; then
+  echo "failed writing cat.grb1.grbml"
   exit 1
 fi
 
-./fiGrbmlCat.sh -o cat.grbml test.grb1.grbml test.grb1.grbml
+./fiGrbmlCat.sh -o cat.grbml cat.grb1.grbml cat.grb1.grbml
 if [ $? != 0 -o ! -f cat.grbml ]; then
   echo "failed writing cat.grbml"
-  rm -f test.grb1.grbml
+  rm -f cat.grb1.grbml
   exit 1
 fi
 
 NC=`grep -c '</gribFileIndex>' cat.grbml`
-rm -f test.grb1.grbml cat.grbml
+rm -f cat.grb1 cat.grb1.grbml cat.grbml
 if [ "$NC" != 1 ]; then
   echo "unexpected count $NC of closing gribFileIndex tags"
   exit 1
