72d4cdfaa4affd4a0929a4e05bfb245b1afe2888
epifanio <epiesasha@me.com>
2019-10-01 10:53:43 +0200

    Update FeltGridDefinition.cpp

--- src/felt/FeltGridDefinition.cpp
+++ src/felt/FeltGridDefinition.cpp
@@ -39,6 +39,10 @@
 #define ACCEPT_USE_OF_DEPRECATED_PROJ_API_H
 #endif
 
+#ifndef ACCEPT_USE_OF_DEPRECATED_PROJ_API_H
+#define ACCEPT_USE_OF_DEPRECATED_PROJ_API_H
+#endif
+
 #include "proj_api.h"
 
 namespace felt {
