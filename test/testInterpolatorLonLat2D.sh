8827b236d95b11106e413e4b813bbca272e6d6ad
Alexander Bürger <alexander.buerger@met.no>
2019-10-29 08:00:08 +0100

    extra test data in tar file, add test for lonlat2d

---
+++ test/testInterpolatorLonLat2D.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+
+TEST="interpolation with 2d lonlat grid mapping"
+echo "testing $TEST"
+
+TEST_NC="${TEST_EXTRADATA_DIR}/grid_mapping_lonlat2d.nc"
+TEST_OC="${TEST_EXTRADATA_DIR}/output-nc4.xml"
+if [ ! -f "${TEST_NC}" -o ! -f "${TEST_OC}" ]; then
+   echo "missing input data '${TEST_NC}' and/or config '${TEST_OC}', skipping test..."
+   exit 0
+fi
+
+OUT="grid_mapping_lonlat2d_interpolated.nc"
+
+./fimex.sh \
+    --input.file "$TEST_NC" \
+    --interpolate.projString "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs" \
+    --interpolate.xAxisUnit m \
+    --interpolate.yAxisUnit m \
+    --interpolate.xAxisValues "520000,525000,...,535000" \
+    --interpolate.yAxisValues "7440000,7441000,...,7448000" \
+    --interpolate.method forward_mean \
+    --output.file "$OUT" \
+    --output.type nc4 \
+    --output.config "$TEST_OC"
+
+E=$?
+if [ $E != 0 ]; then
+  echo "failed $TEST"
+else
+    echo "success"
+fi
+rm -f "$OUT"
+exit $E
