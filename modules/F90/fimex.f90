metno/fimex

commit c9728e82f42e0d4bd54fb8e29b15982667fea79c

vertical interpolation to hybrid axis
 * add ignoreValidityMin/Max options (primarily to keep
   ocean sg test files unchanged)

modules/python/pyfimex0_CoordinateSystem.cc
@@ -103,6 +103,7 @@ void pyfimex0_CoordinateSystem()
             .value("Lat", CoordinateAxis::Lat)
             .value("Pressure", CoordinateAxis::Pressure)
             .value("Height", CoordinateAxis::Height)
+            .value("Dpeth", CoordinateAxis::Depth)
             .value("ReferenceTime", CoordinateAxis::ReferenceTime)
             .value("Realization", CoordinateAxis::Realization)
             ;

include/fimex/CDMVerticalInterpolator.h
@@ -68,8 +68,15 @@ public:
      * @param level1 the new vertical levels, for 'pressure', that is pressure in hPa
      * @param level2 only required for hybrid levels, not yet supported
      */
-    CDMVerticalInterpolator(CDMReader_p dataReader, const std::string& verticalType, const std::string& verticalInterpolationMethod, const std::vector<double>& level1, const std::vector<double>& level2);
-    virtual ~CDMVerticalInterpolator();
+    CDMVerticalInterpolator(CDMReader_p dataReader, const std::string& verticalType, const std::string& verticalInterpolationMethod);
+    ~CDMVerticalInterpolator();
+
+    void ignoreValidityMin(bool ignore);
+    void ignoreValidityMax(bool ignore);
+
+    void interpolateToFixed(const std::vector<double>& level1);
+    void interpolateByTemplateVariable(const std::string& tv);
+
     using CDMReader::getDataSlice;
     /**
      * retrieve data from the underlying dataReader and interpolate the values to the new vertical levels

include/fimex/coordSys/CoordinateAxis.h
@@ -48,6 +48,7 @@ public:
         Lat,
         Pressure,
         Height,
+        Depth,
         ReferenceTime,
         Realization
     };
@@ -61,6 +62,7 @@ public:
         case Lat: return "Lat";
         case Pressure: return "Pressure";
         case Height: return "Height";
+        case Depth: return "Depth";
         case ReferenceTime: return "ReferenceTime";
         case Realization: return "Realization";
         default: return "Undefined";

include/fimex/coordSys/verticalTransform/Depth.h
@@ -1,7 +1,7 @@
 /*
- * Fimex, OceanSG1.h
+ * Fimex, Depth.h
  *
- * (C) Copyright 2013, met.no
+ * (C) Copyright 2018, met.no
  *
  * Project Info:  https://wiki.met.no/fimex/start
  *
@@ -19,64 +19,46 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
- *
- *  Created on: Aug 6, 2013
- *      Author: heikok
  */
 
-#ifndef OCEANSG1_H_
-#define OCEANSG1_H_
+#ifndef DEPTH_H_
+#define DEPTH_H_
 
 #include "fimex/coordSys/verticalTransform/VerticalTransformation.h"
-#include "fimex/coordSys/verticalTransform/OceanSCoordinateGToDepthConverter.h"
 
 namespace MetNoFimex
 {
-/**
- * @headerfile fimex/coordSys/verticalTransform/OceanSG1.h
- */
 
 /**
- * ocean_s_coordinate_g2 is defined by the same set of parameters, just
- * the conversion function is different.
+ * @headerfile fimex/coordSys/verticalTransform/Depth.h
  */
-class OceanSG1 : public VerticalTransformation
+class Depth : public VerticalTransformation
 {
-protected:
-    OceanSCoordinateGToDepthConverter::heightconversion_t heightConversionFunction;
-
 public:
-    const OceanSGVars vars;
-
-    /**
-     * Initialize OceanSG1 with formula as defined by https://www.myroms.org/wiki/index.php/Vertical_S-coordinate
-     *
-     * @param s fractional vertical stretching coordinate sigma(k)
-     * @param C free surface C(x,y,t)
-     * @param depth unperturbed ocean-depth depth(x,y)
-     * @param depth_c critical depth, usually min(depth(x,y))
-     * @param eta time-varying free surface eta(x,y,z) (often also called zeta)
-     */
-    OceanSG1(const OceanSGVars& vars);
-
+    /// name of the depth variable
+    const std::string depth;
+    Depth(const std::string& depth) : depth(depth) {}
+    virtual ~Depth() {}
     /**
      * static NAME constant
-     * @return ocean_s_coordinate_g1
+     * @return depth
      */
-    static const std::string NAME() {return "ocean_s_coordinate_g1";}
+    static const std::string NAME() {return "depth";}
     /**
      * @return same as static NAME()
      */
     virtual std::string getName() const { return NAME(); }
     virtual int getPreferredVerticalType() const { return MIFI_VINT_DEPTH; }
-    virtual std::string getParameterString() const { return "s="+vars.s+",C="+vars.C+",depth="+vars.depth+",depth_c="+vars.depth_c+",eta="+vars.eta; }
-    virtual bool isComplete() const { return vars.isComplete(); }
+    virtual std::string getParameterString() const { return "d="+depth; }
+    virtual bool isComplete() const {return !depth.empty();}
 
 protected:
     VerticalConverterPtr getPressureConverter(CDMReader_p reader, CoordSysPtr cs) const;
+    VerticalConverterPtr getHeightConverter(CDMReader_p reader, CoordSysPtr cs) const;
     VerticalConverterPtr getAltitudeConverter(CDMReader_p reader, CoordSysPtr cs) const;
     VerticalConverterPtr getDepthConverter(CDMReader_p reader, CoordSysPtr cs) const;
 };
 
 } /* namespace MetNoFimex */
-#endif /* OCEANSG1_H_ */
+
+#endif /* DEPTH_H_ */

include/fimex/coordSys/verticalTransform/Height.h
@@ -56,9 +56,9 @@ public:
     virtual bool isComplete() const {return !height.empty();}
 
 protected:
-    virtual VerticalConverterPtr getPressureConverter(CDMReader_p reader, CoordSysPtr cs) const;
-    virtual VerticalConverterPtr getHeightConverter(CDMReader_p reader, CoordSysPtr cs) const;
-    virtual VerticalConverterPtr getAltitudeConverter(CDMReader_p reader, CoordSysPtr cs) const;
+    VerticalConverterPtr getPressureConverter(CDMReader_p reader, CoordSysPtr cs) const;
+    VerticalConverterPtr getHeightConverter(CDMReader_p reader, CoordSysPtr cs) const;
+    VerticalConverterPtr getAltitudeConverter(CDMReader_p reader, CoordSysPtr cs) const;
 };
 
 } /* namespace MetNoFimex */

include/fimex/coordSys/verticalTransform/OceanSCoordinateGToDepthConverter.h
@@ -53,8 +53,10 @@ public:
     std::vector<std::string> getShape() const;
     DataPtr getDataSlice(const SliceBuilder& sb) const;
 
-    std::vector<std::string> getValidityShape(const std::string& verticalDim) const;
-    DataPtr getValiditySlice(const SliceBuilder& sb, const std::vector<double>& verticalValues) const;
+    std::vector<std::string> getValidityMaxShape() const;
+    DataPtr getValidityMax(const SliceBuilder& sb) const;
+    std::vector<std::string> getValidityMinShape() const;
+    DataPtr getValidityMin(const SliceBuilder& sb) const;
 
 private:
     const OceanSGVars vars_;

include/fimex/coordSys/verticalTransform/VerticalConverter.h
@@ -28,15 +28,21 @@ public:
     virtual DataPtr getDataSlice(const SliceBuilder& sb) const = 0;
 
     /** The VLevelConverter usually knows about validity of vertical values at a certain position.
-     *  This function returns bool data (as unsigned char) indicating if the slice index can be valid.
-     *  The vertical dimension of sb will be replaced by a dimension of verticalValues.size().
+     *  This function returns the maximum valid value, e.g. surface pressure for pressure.
      *  If null is returned, everyting is valid.
      */
-    virtual DataPtr getValiditySlice(const SliceBuilder& sb, const std::vector<double>& verticalValues) const = 0;
+    virtual DataPtr getValidityMax(const SliceBuilder& sb) const = 0;
+
+    /** The VLevelConverter usually knows about validity of vertical values at a certain position.
+     *  This function returns the minimum valid value, e.g. 0 for pressure.
+     *  If null is returned, everyting is valid.
+     */
+    virtual DataPtr getValidityMin(const SliceBuilder& sb) const = 0;
 
     /** Return the shape of the validity data.
      */
-    virtual std::vector<std::string> getValidityShape(const std::string& verticalDim) const = 0;
+    virtual std::vector<std::string> getValidityMaxShape() const = 0;
+    virtual std::vector<std::string> getValidityMinShape() const = 0;
 };
 
 typedef boost::shared_ptr<VerticalConverter> VerticalConverterPtr;
@@ -47,8 +53,10 @@ public:
     BasicVerticalConverter(CDMReader_p reader, CoordSysPtr cs)
         : reader_(reader), cs_(cs) { }
 
-    DataPtr getValiditySlice(const SliceBuilder& sb, const std::vector<double>& verticalValues) const;
-    std::vector<std::string> getValidityShape(const std::string& verticalDim) const;
+    DataPtr getValidityMax(const SliceBuilder& sb) const;
+    DataPtr getValidityMin(const SliceBuilder& sb) const;
+    std::vector<std::string> getValidityMaxShape() const;
+    std::vector<std::string> getValidityMinShape() const;
 
 protected:
     CDMReader_p reader_;

include/fimex/coordSys/verticalTransform/VerticalTransformation.h
@@ -134,6 +134,14 @@ protected:
      */
     virtual VerticalConverterPtr getHeightConverter(CDMReader_p reader, CoordSysPtr cs) const;
 
+    /**
+     * Default implementation: No conversion.
+     * @param reader
+     * @param cs
+     * @return
+     */
+    virtual VerticalConverterPtr getDepthConverter(CDMReader_p reader, CoordSysPtr cs) const;
+
 private:
     VerticalConverterPtr getIdentityPressureConverter(CDMReader_p reader, CoordSysPtr cs) const;
     VerticalConverterPtr findPressureConverter(CDMReader_p reader, CoordSysPtr cs) const;

commit 899366d10d5d62e16204cd2fa4958ee679767f93

bugfix axis types
 * regression in commit c9728e82 (vertical interpolation to hybrid axis)

modules/F90/fimex.f90
@@ -29,7 +29,7 @@ MODULE Fimex
   !! These are the same definitions as in CoordinateAxis::AxisType
   ENUM, BIND(C)
     ENUMERATOR :: AXIS_Undefined = 0, AXIS_GeoX, AXIS_GeoY, AXIS_GeoZ, AXIS_Time, AXIS_Lon, AXIS_Lat,&
-     AXIS_Pressure, AXIS_Height, AXIS_ReferenceTime, AXIS_Realization
+     AXIS_Pressure, AXIS_Height, AXIS_Depth, AXIS_ReferenceTime, AXIS_Realization
   END ENUM
   !> Interpolation methods
   !! These are the same definintions as #mifi_interpol_method in mifi_constants.h

modules/python/pyfimex0_CoordinateSystem.cc
@@ -77,7 +77,7 @@ void pyfimex0_CoordinateSystem(py::module m)
         .value("Lat", CoordinateAxis::Lat)
         .value("Pressure", CoordinateAxis::Pressure)
         .value("Height", CoordinateAxis::Height)
-        .value("Dpeth", CoordinateAxis::Depth)
+        .value("Depth", CoordinateAxis::Depth)
         .value("ReferenceTime", CoordinateAxis::ReferenceTime)
         .value("Realization", CoordinateAxis::Realization);
 