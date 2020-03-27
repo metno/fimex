metno/fimex

commit 0502f0c8f38ee22c69f7f88537bb1d3d497188e1

BOOST_FOREACH => for ( : )

src/binSrc/fimex.cc
@@ -43,17 +43,20 @@
 #include "fimex/TimeUnit.h"
 #include "fimex/Utils.h"
 #include "fimex/coordSys/CoordinateSystem.h"
+
 #include "fimex_config.h"
+
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/program_options.hpp>
-#include <boost/regex.hpp>
 #include <boost/tokenizer.hpp>
-#include <boost/version.hpp>
+
 #include <cctype>
 #include <fstream>
 #include <iostream>
 #include <memory>
 #include <numeric>
+#include <regex>
+
 #ifdef HAVE_LOG4CPP
 #include "log4cpp/PropertyConfigurator.hh"
 #endif
@@ -274,8 +277,8 @@ string getType(const string& io, const po::variables_map& vm)
             file = ".fillFile";
         }
         if (file != "") {
-            boost::smatch what;
-            if (boost::regex_match(vm[io+file].as<string>(), what, boost::regex(".*\\.(\\w+)$"))) {
+            std::smatch what;
+            if (std::regex_match(vm[io + file].as<string>(), what, std::regex(".*\\.(\\w+)$"))) {
                 type = what[1].str();
             }
         }
@@ -639,29 +642,29 @@ CDMReader_p getCDMVerticalInterpolator(const po::variables_map& vm, CDMReader_p 
 
 std::shared_ptr<InterpolatorProcess2d> parseProcess(const string& procString, const string& logProcess)
 {
-    boost::smatch what;
-     if (boost::regex_match(procString, what, boost::regex("\\s*fill2d\\(([^,]+),([^,]+),([^)]+)\\).*"))) {
-         double critx = string2type<double>(what[1]);
-         double cor = string2type<double>(what[2]);
-         size_t maxLoop = string2type<size_t>(what[3]);
-         LOG4FIMEX(logger, Logger::DEBUG, "running interpolate "<< logProcess <<": fill2d("<<critx<<","<<cor<<","<<maxLoop<<")");
-         return std::make_shared<InterpolatorFill2d>(critx, cor, maxLoop);
-     } else if (boost::regex_match(procString, what, boost::regex("\\s*creepfill2d\\((.+)\\).*"))) {
-         vector<string> vals = tokenize(what[1], ",");
-         if (vals.size() == 2) {
-             unsigned short repeat = string2type<unsigned short>(vals.at(0));
-             char setWeight = string2type<char>(vals.at(1));
-             LOG4FIMEX(logger, Logger::DEBUG, "running interpolate "<< logProcess <<": creepfill2d("<<repeat<<","<<setWeight<<")");
-             return std::make_shared<InterpolatorCreepFill2d>(repeat, setWeight);
-         } else if (vals.size() == 3) {
-             unsigned short repeat = string2type<unsigned short>(vals.at(0));
-             char setWeight = string2type<char>(vals.at(1));
-             float defVal = string2type<float>(vals.at(2));
-             LOG4FIMEX(logger, Logger::DEBUG, "running interpolate "<< logProcess <<": creepfillval2d("<<repeat<<","<<setWeight<<","<<defVal<<")");
-             return std::make_shared<InterpolatorCreepFillVal2d>(repeat, setWeight, defVal);
-         } else {
-             throw CDMException("creepfill requires two or three arguments, got " + what[1]);
-         }
+    std::smatch what;
+    if (std::regex_match(procString, what, std::regex("\\s*fill2d\\(([^,]+),([^,]+),([^)]+)\\).*"))) {
+        double critx = string2type<double>(what[1]);
+        double cor = string2type<double>(what[2]);
+        size_t maxLoop = string2type<size_t>(what[3]);
+        LOG4FIMEX(logger, Logger::DEBUG, "running interpolate " << logProcess << ": fill2d(" << critx << "," << cor << "," << maxLoop << ")");
+        return std::make_shared<InterpolatorFill2d>(critx, cor, maxLoop);
+    } else if (std::regex_match(procString, what, std::regex("\\s*creepfill2d\\((.+)\\).*"))) {
+        vector<string> vals = tokenize(what[1], ",");
+        if (vals.size() == 2) {
+            unsigned short repeat = string2type<unsigned short>(vals.at(0));
+            char setWeight = string2type<char>(vals.at(1));
+            LOG4FIMEX(logger, Logger::DEBUG, "running interpolate " << logProcess << ": creepfill2d(" << repeat << "," << setWeight << ")");
+            return std::make_shared<InterpolatorCreepFill2d>(repeat, setWeight);
+        } else if (vals.size() == 3) {
+            unsigned short repeat = string2type<unsigned short>(vals.at(0));
+            char setWeight = string2type<char>(vals.at(1));
+            float defVal = string2type<float>(vals.at(2));
+            LOG4FIMEX(logger, Logger::DEBUG, "running interpolate " << logProcess << ": creepfillval2d(" << repeat << "," << setWeight << "," << defVal << ")");
+            return std::make_shared<InterpolatorCreepFillVal2d>(repeat, setWeight, defVal);
+        } else {
+            throw CDMException("creepfill requires two or three arguments, got " + what[1].str());
+        }
      }
      throw CDMException("undefined interpolate."+logProcess+": " + procString);
 }
@@ -808,8 +811,8 @@ CDMReader_p getCDMMerger(const po::variables_map& vm, CDMReader_p dataReader)
     if( vm.count("merge.smoothing") ) {
         const string& v = vm["merge.smoothing"].as<string>();
         if (boost::starts_with(v, "LINEAR")) {
-            boost::smatch what;
-            if (boost::regex_match(v, what, boost::regex("^LINEAR\\(([^,]+),([^,]+)\\)$"))) {
+            std::smatch what;
+            if (std::regex_match(v, what, std::regex("^LINEAR\\(([^,]+),([^,]+)\\)$"))) {
                 try {
                     int transition = boost::lexical_cast<int>(what[1]);
                     int border     = boost::lexical_cast<int>(what[2]);

commit 4494bd2e592be72ee4c394446da02c3d1e3d967c

bugfix fimex.cc / testInterpolatorFill.sh
 * regression in commit 0502f0c8
 * need to skip spaces in parseProcess
 * testInterpolatorFill.sh was never run when building outside source
   because the test data were not found

src/binSrc/fimex.cc
@@ -661,7 +661,7 @@ CDMReader_p getCDMVerticalInterpolator(const po::value_set& vm, CDMReader_p data
 std::shared_ptr<InterpolatorProcess2d> parseProcess(const string& procString, const string& logProcess)
 {
     std::smatch what;
-    if (std::regex_match(procString, what, std::regex("\\s*fill2d\\(([^,]+),([^,]+),([^)]+)\\).*"))) {
+    if (std::regex_match(procString, what, std::regex("\\s*fill2d\\(([^, ]+), *([^, ]+), *([^ )]+)\\).*"))) {
         double critx = string2type<double>(what[1]);
         double cor = string2type<double>(what[2]);
         size_t maxLoop = string2type<size_t>(what[3]);

test/fillInterpolator.cfg
@@ -1,9 +1,3 @@
-[input]
-file=coordTest.nc
-
-[output]
-file=coordTestFilled.nc
-
 [interpolate]
 method=nearestneighbor
 projString = +proj=stere +lat_0=90 +lon_0=0 +lat_ts=60 +units=m +a=6.371e+06 +e=0 +no_defs

test/testInterpolatorFill.sh
@@ -1,25 +1,46 @@
-#! /bin/sh
-if [ ! -f coordTest.nc ]; then
-	exit 0;
+#!/bin/sh
+
+TEST_SRCDIR=`dirname $0`
+TEST_BINDIR=`pwd`
+
+cd "$TEST_SRCDIR"
+
+INPUT_FILE="coordTest.nc"
+OUTPUT_FILE="$TEST_BINDIR/coordTestFilled.nc"
+
+if [ ! -f "$INPUT_FILE" ]; then
+    echo "missing test data file '$INPUT_FILE'"
+    exit 1;
 fi
-echo "testing interpolator with prefill"
-./fimex.sh -c fillInterpolator.cfg
+
+TESTCASE="interpolator with prefill"
+echo "testing $TESTCASE"
+"${TEST_BINDIR}/fimex.sh" \
+    -c fillInterpolator.cfg \
+    --input.file "$INPUT_FILE" \
+    --output.file "$OUTPUT_FILE"
 if [ $? != 0 ]; then
-  echo "failed fillInterpolate"
+  echo "failed $TESTCASE"
   exit 1
 fi
-echo "success"
-echo "testing interpolator with postfill"
-./fimex.sh --input.file=coordTest.nc --output.file=coordTestFilled.nc \
-   --interpolate.method=nearestneighbor --interpolate.projString=\
-"+proj=stere +lat_0=90 +lon_0=0 +lat_ts=60 +units=m +a=6.371e+06 +e=0 +no_defs" \
---interpolate.preprocess="fill2d(0.01, 1.6, 1000)" \
---interpolate.xAxisValues="-1705516, -1655353, -1605191, -1555029, -1504867, -1454704, -1404542,-1354380, -1304218, -1254056, -1203893" \
---interpolate.yAxisValues="-6872225, -6822063, -6771901, -6721738, -6671576, -6621414, -6571252,-6521089, -6470927, -6420765, -6370603" \
---interpolate.xAxisUnit=m --interpolate.yAxisUnit=m
+echo "success $TESTCASE"
+
+TESTCASE="interpolator with postfill"
+echo "testing $TESTCASE"
+"${TEST_BINDIR}/fimex.sh" \
+    --input.file "$INPUT_FILE" \
+    --output.file "$OUTPUT_FILE" \
+    --interpolate.method nearestneighbor \
+    --interpolate.projString "+proj=stere +lat_0=90 +lon_0=0 +lat_ts=60 +units=m +a=6.371e+06 +e=0 +no_defs" \
+    --interpolate.preprocess "fill2d(0.01, 1.6, 1000)" \
+    --interpolate.xAxisValues "-1705516, -1655353, -1605191, -1555029, -1504867, -1454704, -1404542,-1354380, -1304218, -1254056, -1203893" \
+    --interpolate.yAxisValues "-6872225, -6822063, -6771901, -6721738, -6671576, -6621414, -6571252,-6521089, -6470927, -6420765, -6370603" \
+    --interpolate.xAxisUnit m \
+    --interpolate.yAxisUnit m
 if [ $? != 0 ]; then
-  echo "failed post-fillInterpolate"
+  echo "failed $TESTCASE"
   exit 1
 fi
-echo "success"
+echo "success $TESTCASE"
+
 exit 0