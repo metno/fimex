metno/fimex

commit 2ce00fbe5785871e7999aa01ab04fca8585e1fc7

catch some exceptions in NetCDF_CDMWriter inside OpenMP parallel

src/NetCDF_CDMWriter.cc
@@ -698,16 +698,28 @@ void NetCDF_CDMWriter::writeData(const NcVarIdMap& ncVarMap)
             const bool no_unlim = (unLimDimPos == -1 && unLimDimIdx == -1 && !cdm.hasUnlimitedDim(cdmVar));
             const bool with_unlim = (unLimDimPos != -1 && unLimDimIdx >= 0 && cdm.hasUnlimitedDim(cdmVar));
             DataPtr data;
-            if (no_unlim) {
-                data = cdmReader->getData(varName);
-            } else if (with_unlim) {
-                data = cdmReader->getDataSlice(varName, unLimDimPos);
-            } else {
-                continue; // FIXME
+            try {
+                if (no_unlim) {
+                    data = cdmReader->getData(varName);
+                } else if (with_unlim) {
+                    data = cdmReader->getDataSlice(varName, unLimDimPos);
+                } else {
+                    continue; // FIXME
+                }
+                if (data)
+                    data = convertData(cdmVar, data);
+            } catch (CDMException& ex) {
+                std::ostringstream msg;
+                msg << "exception while reading variable '" << varName << "'";
+                if (!no_unlim)
+                    msg << " at unlimited dim position " << unLimDimPos;
+                msg << "; using _FillValue";
+                msg << "; message: " << ex.what();
+                LOG4FIMEX(logger, Logger::ERROR, msg.str());
+                data = nullptr;
             }
-            data = convertData(cdmVar, data);
 
-            if (data->size() == 0 && ncFile->format < 3) {
+            if ((!data || data->size() == 0) && ncFile->format < 3) {
                 // need to write data with _FillValue,
                 // since we are using NC_NOFILL for nc3 format files = NC_FORMAT_CLASSIC(1) NC_FORMAT_64BIT(2))
                 if (with_unlim)
@@ -715,13 +727,13 @@ void NetCDF_CDMWriter::writeData(const NcVarIdMap& ncVarMap)
                 size_t size = (n_dims > 0) ? std::accumulate(count.get(), count.get() + n_dims, 1, std::multiplies<size_t>()) : 1;
                 data = createData(cdmVar.getDataType(), size, cdm.getFillValue(varName));
             }
-            if (data->size() > 0) {
+            if (data && data->size() > 0) {
                 if (with_unlim) {
                     count[unLimDimIdx] = 1;
                     start[unLimDimIdx] = unLimDimPos;
                 }
                 LOG4FIMEX(logger, Logger::DEBUG,
-                          "writing variable " << varName << "dimLen= " << n_dims << " start=" << join(&start[0], &start[0] + n_dims)
+                          "writing variable " << varName << " dimLen= " << n_dims << " start=" << join(&start[0], &start[0] + n_dims)
                                               << " count=" << join(&count[0], &count[0] + n_dims));
                 OmpScopedLock ncLock(Nc::getMutex());
                 try {

commit d068c977f4f5bf5977aa759cbae563c216b328ab

bugfix NetCDF_CDMWriter
 * regression in commit 2ce00fbe
 * stop writing netcdf in case of exceptions, and throw a new
   exception after the end of the OpenMP write loop

src/NetCDF_CDMWriter.cc
@@ -629,11 +629,12 @@ void NetCDF_CDMWriter::writeData(const NcVarIdMap& ncVarMap)
     // see http://www.unidata.ucar.edu/support/help/MailArchives/netcdf/msg10905.html
     // use unLimDimPos = -1 for variables without unlimited dimension
 
+    bool exceptions = false;
 #ifdef _OPENMP
 #if (defined(__GNUC__) && __GNUC__ >= 9) || defined(__clang__)
-#pragma omp parallel for default(none) shared(logger, cdmVars, ncVarMap, maxUnLim, unLimDimId)
+#pragma omp parallel for default(none) shared(logger, cdmVars, ncVarMap, maxUnLim, unLimDimId, exceptions)
 #elif !defined(__INTEL_COMPILER) || (__INTEL_COMPILER >= 1800)
-#pragma omp parallel for default(none) shared(logger, cdmVars, ncVarMap)
+#pragma omp parallel for default(none) shared(logger, cdmVars, ncVarMap, exceptions)
 #endif // __INTEL_COMPILER
 #endif // _OPENMP
     for (long long unLimDimPos = -1; unLimDimPos < maxUnLim; ++unLimDimPos) {
@@ -651,6 +652,8 @@ void NetCDF_CDMWriter::writeData(const NcVarIdMap& ncVarMap)
         }
 #endif
         for (size_t vi = 0; vi < cdmVars.size(); ++vi) {
+            if (exceptions)
+                continue;
             const CDMVariable& cdmVar = cdmVars[vi];
             const std::string& varName = cdmVar.getName();
             const int varId = ncVarMap.find(varName)->second;
@@ -713,11 +716,23 @@ void NetCDF_CDMWriter::writeData(const NcVarIdMap& ncVarMap)
                 msg << "exception while reading variable '" << varName << "'";
                 if (!no_unlim)
                     msg << " at unlimited dim position " << unLimDimPos;
-                msg << "; using _FillValue";
+                msg << "; will stop writing data";
                 msg << "; message: " << ex.what();
                 LOG4FIMEX(logger, Logger::ERROR, msg.str());
                 data = nullptr;
+                exceptions = true;
+            } catch (...) {
+                std::ostringstream msg;
+                msg << "exception while reading variable '" << varName << "'";
+                if (!no_unlim)
+                    msg << " at unlimited dim position " << unLimDimPos;
+                msg << "; will stop writing data";
+                LOG4FIMEX(logger, Logger::ERROR, msg.str());
+                data = nullptr;
+                exceptions = true;
             }
+            if (exceptions)
+                continue;
 
             if ((!data || data->size() == 0) && ncFile->format < 3) {
                 // need to write data with _FillValue,
@@ -753,6 +768,8 @@ void NetCDF_CDMWriter::writeData(const NcVarIdMap& ncVarMap)
         }
 #endif
     }
+    if (exceptions)
+        throw CDMException("netcdf writing failed with ERRORs");
 }
 
 void NetCDF_CDMWriter::init()

src/binSrc/fimex.cc
@@ -888,9 +888,6 @@ void writeCDM(CDMReader_p dataReader, const po::value_set& vm)
     const string config = getConfig("output", vm);
     try {
       createWriter(dataReader, type, fileName, config);
-    } catch (CDMException& ex) {
-      LOG4FIMEX(logger, Logger::FATAL, "CDMException while writing: " << ex.what());
-      exit(1);
     } catch (exception& ex) {
       LOG4FIMEX(logger, Logger::FATAL, "exception while writing: " << ex.what());
       exit(1);