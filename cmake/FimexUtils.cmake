metno/fimex

commit ef930a19fc744172737f5a088682e1f5121a56b2

rewrite FIMEX_FIND_PACKAGE
 * add FindPkgConfigBugfixLibraryPaths.cmake, from cmake git to
   solve bad handling of -L for IMPORTED before cmake 3.12
   (cmake issue 17954)

 * name of cmake-config imported target for netCDF changed between
   bionic and focal

 * libfelt needs proj

cmake/FimexUtils.cmake
@@ -42,7 +42,12 @@ MACRO(FIMEX_CMAKE_SETUP)
 
   INCLUDE(CMakePackageConfigHelpers)
   INCLUDE(GNUInstallDirs)
-  INCLUDE(FindPkgConfig)
+  IF (${CMAKE_VERSION} VERSION_LESS "3.12.0")
+    # see https://github.com/Kitware/CMake/commit/ac5731a7e380349f19dc319e6c31e189b5faba93
+    INCLUDE(FindPkgConfigBugfixLibraryPaths)
+  ELSE ()
+    INCLUDE(FindPkgConfig)
+  ENDIF ()
 
   SET(CMAKE_CXX_STANDARD 11)
   SET(CMAKE_CXX_STANDARD_REQUIRED ON)
@@ -144,11 +149,12 @@ FUNCTION(FIMEX_HEADERS headers sources source_suffix header_suffix)
 ENDFUNCTION()
 
 
-FUNCTION(FIMEX_ADD_LIBRARY name sources libs includes options)
+FUNCTION(FIMEX_ADD_LIBRARY name sources packages)
   IF(BUILD_SHARED_LIBS)
+    MESSAGE(STATUS "adding shared lib '${name}' using packages '${packages}'")
     SET(shared_lib lib${name})
     ADD_LIBRARY(${shared_lib} SHARED ${sources})
-    TARGET_LINK_LIBRARIES(${shared_lib} PRIVATE ${libs})
+    TARGET_LINK_LIBRARIES(${shared_lib} PRIVATE ${packages})
     TARGET_INCLUDE_DIRECTORIES(${shared_lib}
       PUBLIC
       $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/include>
@@ -156,7 +162,6 @@ FUNCTION(FIMEX_ADD_LIBRARY name sources libs includes options)
       PRIVATE
       $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>
       $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
-      ${includes}
       )
     IF(options)
       TARGET_COMPILE_OPTIONS(${shared_lib} PUBLIC ${options})
@@ -184,7 +189,6 @@ FUNCTION(FIMEX_ADD_LIBRARY name sources libs includes options)
       PRIVATE
       $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>
       $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
-      ${includes}
       )
     SET_TARGET_PROPERTIES(${static_lib} PROPERTIES
       OUTPUT_NAME "${name}${MINUS_FIMEX_VERSION}"
@@ -201,48 +205,110 @@ ENDFUNCTION()
 INCLUDE(CheckFunctionExists)
 
 FUNCTION(CHECK_NETCDF_HAS_HDF5 found)
-  IF (netcdf_PC)
-    # we want only netcdf
-    PKG_CHECK_MODULES(PC_NETCDF QUIET REQUIRED ${netcdf_PC})
-    SET(CMAKE_REQUIRED_FLAGS ${PC_NETCDF_LDFLAGS})
-    SET(CMAKE_REQUIRED_DEFINITIONS ${PC_NETCDF_CFLAGS_OTHER})
-    SET(CMAKE_REQUIRED_INCLUDES ${PC_NETCDF_INCLUDE_DIRS})
-    SET(CMAKE_REQUIRED_LIBRARIES ${PC_NETCDF_LIBRARIES})
-  ELSE ()
-    SET(CMAKE_REQUIRED_INCLUDES ${netcdf_INC_DIR})
-    SET(CMAKE_REQUIRED_LIBRARIES ${netcdf_LIB})
-  ENDIF ()
+  SET(CMAKE_REQUIRED_FLAGS "")
+  SET(CMAKE_REQUIRED_DEFINITIONS "")
+  SET(CMAKE_REQUIRED_INCLUDES "")
+  SET(CMAKE_REQUIRED_LIBRARIES ${netCDF_PACKAGE})
   CHECK_FUNCTION_EXISTS("nc_def_var_deflate" ${found})
 ENDFUNCTION()
 
+FUNCTION(FIMEX_ADD_IMPORTED_LIBRARY name libraries includes definitions)
+  add_library(${name} INTERFACE IMPORTED)
+  set_target_properties(
+    ${name}
+    PROPERTIES
+      INTERFACE_LINK_LIBRARIES "${libraries}"
+      INTERFACE_INCLUDE_DIRECTORIES "${includes}"
+      INTERFACE_COMPILE_DEFINITIIONS "${definitions}"
+  )
+ENDFUNCTION()
 
-FUNCTION(FIMEX_FIND_PACKAGE name
-    pkg_pc
-    pkg_libname
-    pkg_hdr
-    )
+FUNCTION(FIMEX_FIND_PACKAGE ffp)
+  set(ARGS0
+    # options
+  )
+  set(ARGS1
+    # arguments with one value
+    PACKAGE
+    VERSION_MIN
+    CMAKE_NAME
+    PKGCONFIG_NAME
+    INCLUDE_HDR
+    LIBRARY_NAME
+  )
+  set(ARGSN
+    # arguments with multiple values
+    CMAKE_TARGETS
+  )
+  CMAKE_PARSE_ARGUMENTS(_ffp "${ARGS0}" "${ARGS1}" "${ARGSN}" ${ARGN})
 
-  UNSET(p_pc_FOUND CACHE)
+  # try to find via cmake
+  IF (_ffp_CMAKE_NAME)
+    FIND_PACKAGE(${_ffp_CMAKE_NAME} ${_ffp_VERSION_MIN} QUIET)
+    IF(${_ffp_CMAKE_NAME}_FOUND)
+      IF (NOT(DEFINED _ffp_CMAKE_TARGETS))
+        SET(_ffp_CMAKE_TARGET ${_ffp_CMAKE_NAME})
+      ENDIF()
+      FOREACH(_ffp_target ${_ffp_CMAKE_TARGETS})
+        IF(TARGET ${_ffp_target})
+          GET_TARGET_PROPERTY(_ffp_is_imported "${_ffp_target}" IMPORTED)
+          IF(_ffp_is_imported)
+            SET(${ffp}_PACKAGE "${_ffp_target}" PARENT_SCOPE)
+            MESSAGE(STATUS "Found ${ffp} via cmake imported")
+            RETURN()
+          ENDIF()
+        ENDIF()
+      ENDFOREACH ()
 
-  IF(pkg_pc)
-    PKG_CHECK_MODULES(p_pc QUIET "${pkg_pc}")
-  ENDIF()
-  IF(p_pc_FOUND)
-    MESSAGE(STATUS "Found ${name}: pkg-config '${pkg_pc}'")
-    SET(${name}_PC ${pkg_pc} PARENT_SCOPE)
-  ELSE()
-    FIND_PATH(${name}_INC_DIR
-      ${pkg_hdr}
-      HINTS "${${name}_INCLUDE_DIR}" "${${name}_DIR}/include"
-    )
-    FIND_LIBRARY(${name}_LIB
-      NAMES ${pkg_libname}
-      HINTS "${${name}_LIB_DIR}" "${${name}_DIR}/lib"
-    )
-    IF((${name}_INC_DIR) AND (${name}_LIB))
-      MESSAGE(STATUS "Found ${name}: include: '${${name}_INC_DIR}/${pkg_hdr}'  library: '${${name}_LIB}'")
-    ELSE()
-      MESSAGE(FATAL_ERROR "Required ${name} include/library not found")
+      FIMEX_ADD_IMPORTED_LIBRARY(${ffp}_IMP
+        "${${_ffp_CMAKE_TARGET}_LIBRARIES}"
+        "${${_ffp_CMAKE_TARGET}_INCLUDE_DIRS}"
+        "${${_ffp_CMAKE_TARGET}_DEFINITIONS}"
+      )
+      SET(${ffp}_PACKAGE "${ffp}_IMP" PARENT_SCOPE)
+      MESSAGE(STATUS "Found ${ffp} via cmake")
+      RETURN()
     ENDIF()
   ENDIF()
+
+
+  # try to find via pkg-config
+  IF(_ffp_PKGCONFIG_NAME)
+    IF(_ffp_VERSION_MIN)
+      SET(_ffp_pc_version "${_ffp_PKGCONFIG_NAME}>=${_ffp_VERSION_MIN}")
+    ELSE()
+      SET(_ffp_pc_version "${_ffp_PKGCONFIG_NAME}")
+    ENDIF()
+    SET (_ffp_pc ${_ffp_PKGCONFIG_NAME})
+    UNSET(${_ffp_pc}_FOUND CACHE)
+    PKG_CHECK_MODULES(${_ffp_pc} IMPORTED_TARGET QUIET "${_ffp_pc_version}")
+    IF(${_ffp_pc}_FOUND)
+      SET(${ffp}_PACKAGE "PkgConfig::${_ffp_pc}" PARENT_SCOPE)
+      MESSAGE(STATUS "Found ${ffp} via pkg-config '${_ffp_pc_version}'")
+      RETURN()
+    ENDIF()
+  ENDIF()
+
+  # try to find via library and header
+  UNSET(_ffp_inc_dir CACHE)
+  FIND_PATH(_ffp_inc_dir
+    ${_ffp_INCLUDE_HDR}
+    HINTS "${${ffp}_INCLUDE_DIR}" "${${ffp}_DIR}/include"
+  )
+  IF (NOT _ffp_inc_dir)
+    MESSAGE(FATAL_ERROR "Cannot find ${ffp}, include header '${_ffp_INCLUDE_HDR}' not found")
+  ENDIF()
+  IF (_ffp_LIBRARY_NAME)
+    UNSET(_ffp_lib CACHE)
+    FIND_LIBRARY(_ffp_lib
+      NAMES ${_ffp_LIBRARY_NAME}
+      HINTS "${${ffp}_LIB_DIR}" "${${ffp}_DIR}/lib"
+    )
+    IF (NOT _ffp_lib)
+      MESSAGE(FATAL_ERROR "Cannot find ${ffp}, library '${_ffp_LIBRARY_NAME}' not found")
+    ENDIF()
+  ENDIF()
+  FIMEX_ADD_IMPORTED_LIBRARY(${ffp}_LIB_INC "${_ffp_lib}" "${_ffp_inc_dir}" "")
+  SET(${ffp}_PACKAGE "${ffp}_LIB_INC" PARENT_SCOPE)
+  MESSAGE(STATUS "Found ${ffp} via find_package/find_path, lib='${_ffp_lib}', include='${_ffp_inc_dir}'")
 ENDFUNCTION()

CMakeLists.txt
@@ -35,14 +35,27 @@ INCLUDE(FimexUtils)
 FIMEX_CMAKE_SETUP()
 FIMEX_VERSION_DEFINES(FIMEX "include/fimex/CDMconstants.h")
 
-FIMEX_FIND_PACKAGE("libxml2" "libxml-2.0>=2.6.0" "xml2" "libxml/xmlversion.h")
-LIST(APPEND FIMEX_PC_REQUIRES_PRIVATE ${libxml2_PC})
+FIMEX_FIND_PACKAGE(libxml2
+  VERSION_MIN "2.6.0"
+  CMAKE_NAME "libxml2"
+  PKGCONFIG_NAME "libxml-2.0"
+  LIBRARY_NAME "xml2"
+  INCLUDE_HDR "libxml/xmlversion.h"
+)
 
-FIMEX_FIND_PACKAGE("udunits2" "udunits>=2" "udunits2" "udunits2.h")
-LIST(APPEND FIMEX_PC_REQUIRES_PRIVATE ${udunits2_PC})
+FIMEX_FIND_PACKAGE(udunits2
+  VERSION_MIN "2"
+  PKGCONFIG_NAME "udunits"
+  LIBRARY_NAME "udunits2"
+  INCLUDE_HDR "udunits2.h"
+)
 
-FIMEX_FIND_PACKAGE("proj" "proj" "proj" "proj_api.h")
-LIST(APPEND FIMEX_PC_REQUIRES_PRIVATE ${proj_PC})
+FIMEX_FIND_PACKAGE(proj
+  CMAKE_NAME "PROJ"
+  PKGCONFIG_NAME "proj"
+  LIBRARY_NAME "proj"
+  INCLUDE_HDR "proj_api.h"
+)
 
 OPTION(BUILD_SHARED_LIBS "Select ON (default) for shared lib, OFF for static lib, BOTH for both" ON)
 
@@ -72,13 +85,25 @@ ENDIF()
 
 OPTION(ENABLE_LOG4CPP "Use Log4Cpp" OFF)
 IF(ENABLE_LOG4CPP)
-  LIST(APPEND FIMEX_PC_REQUIRES_PRIVATE log4cpp)
+  FIMEX_FIND_PACKAGE(log4cpp
+    PKGCONFIG_NAME "log4cpp"
+    LIBRARY_NAME "liblog4cpp"
+    INCLUDE_HDR "log4cpp/Priority.hh"
+  )
 ENDIF()
 
 OPTION(ENABLE_NETCDF "Use NetCDF" ON)
 IF(ENABLE_NETCDF)
-  FIMEX_FIND_PACKAGE("netcdf" "netcdf" "netcdf" "netcdf.h")
-  LIST(APPEND FIMEX_PC_REQUIRES_PRIVATE ${netcdf_PC})
+  FIMEX_FIND_PACKAGE(netCDF
+    CMAKE_NAME "netCDF"
+    CMAKE_TARGETS
+      "netCDF::netcdf" # focal, debian sid, ...
+      "netcdf"         # bionic
+    PKGCONFIG_NAME "netcdf"
+    LIBRARY_NAME "netcdf"
+    INCLUDE_HDR "netcdf.h"
+  )
+  CHECK_NETCDF_HAS_HDF5(HAVE_NETCDF_HDF5_LIB)
 ENDIF()
 
 OPTION(ENABLE_FELT "Use Felt library" ON)
@@ -90,7 +115,7 @@ IF((ENABLE_GRIBAPI) AND (ENABLE_ECCODES))
   MESSAGE(FATAL_ERROR "Cannot use both ecCodes libraries")
 ELSEIF(ENABLE_ECCODES)
   FIND_PACKAGE(eccodes REQUIRED)
-  SET(eccodes_LIB "eccodes")
+  SET(eccodes_PACKAGE "eccodes")
   MESSAGE(STATUS "Detected ecCodes version ${eccodes_VERSION}")
   # cmake variable "eccodes_FEATURES" was called "ECCODES_FEATURES" before 2.13.0
   IF((NOT eccodes_FEATURES) AND (ECCODES_FEATURES))
@@ -102,7 +127,10 @@ ELSEIF(ENABLE_ECCODES)
     SET(HAVE_GRIB_THREADSAFE 1)
   ENDIF()
 ELSEIF(ENABLE_GRIBAPI)
-  FIMEX_FIND_PACKAGE("grib_api" "" "grib_api" "grib_api.h")
+  FIMEX_FIND_PACKAGE(grib_api_PACKAGE
+    LIBRARY_NAME "grib_api"
+    INCLUDE_HDR "grib_api.h"
+  )
   OPTION(ENABLE_GRIBAPIPTHREAD "Use GribAPI library with pthread support" ON)
   IF(ENABLE_GRIBAPIPTHREAD)
     SET(HAVE_GRIB_THREADSAFE 1)
@@ -111,12 +139,20 @@ ENDIF()
 
 OPTION(ENABLE_METGM "Use metgm" OFF)
 IF(ENABLE_METGM)
-  LIST(APPEND FIMEX_PC_REQUIRES_PRIVATE metgm)
+  FIMEX_FIND_PACKAGE(metgm
+    PKGCONFIG_NAME "metgm"
+    LIBRARY_NAME "libmetgm"
+    INCLUDE_HDR "metgm.h"
+  )
 ENDIF()
 
 OPTION(ENABLE_PRORADXML "Use proradxml" OFF)
 IF(ENABLE_PRORADXML)
-  LIST(APPEND FIMEX_PC_REQUIRES_PRIVATE libproradxmlrw)
+  FIMEX_FIND_PACKAGE(proradxmlrw
+    PKGCONFIG_NAME "libproradxmlrw"
+    LIBRARY_NAME "libproradxmlrw"
+    INCLUDE_HDR "proradxmlrw.h"
+  )
 ENDIF()
 
 OPTION(ENABLE_FORTRAN "Use fortran" OFF)
@@ -126,12 +162,6 @@ ENDIF()
 
 OPTION(ENABLE_FIMEX_VERSIONNUMBERED "Use version numbers in filenames" ON)
 
-PKG_CHECK_MODULES(PC REQUIRED ${FIMEX_PC_REQUIRES};${FIMEX_PC_REQUIRES_PRIVATE})
-
-IF(ENABLE_NETCDF)
-  CHECK_NETCDF_HAS_HDF5(HAVE_NETCDF_HDF5_LIB)
-ENDIF()
-
 # github/HowardHinnant/date
 IF (NOT date_INC_DIR)
   SET (DATE_3RDPARTY "third_party/date")
@@ -139,12 +169,18 @@ IF (NOT date_INC_DIR)
     SET (date_INC_DIR "${CMAKE_SOURCE_DIR}/${DATE_3RDPARTY}/include")
   ELSE ()
     FIND_PACKAGE(date REQUIRED)
-    SET (date_LIB date_interface) # just a header file, therefore '..._interface'
+    SET (date_PACKAGE date_interface) # just a header file, therefore '..._interface'
   ENDIF ()
-ELSEIF (NOT (EXISTS "${date_INC_DIR}/date/date.h"))
-  MESSAGE (SEND_ERROR "Missing '${date_INC_DIR}/date/date.h'")
-ELSE()
-  MESSAGE (STATUS "Using '${date_INC_DIR}/date/date.h'")
+ENDIF ()
+IF (NOT date_PACKAGE)
+  SET(date_HDR "${date_INC_DIR}/date/date.h")
+  IF (EXISTS "${date_HDR}")
+    MESSAGE (STATUS "Using '${date_HDR}'")
+    FIMEX_ADD_IMPORTED_LIBRARY(date_IMP "" "${date_INC_DIR}" "")
+    SET (date_PACKAGE date_IMP)
+  ELSE()
+    MESSAGE (SEND_ERROR "Missing '${date_HDR}'")
+  ENDIF ()
 ENDIF ()
 
 SET (MI_PROGRAMOPTIONS_3RDPARTY "third_party/mi-programoptions")
@@ -173,8 +209,8 @@ SET(FIMEX_INSTALL_INCLUDEDIR "${CMAKE_INSTALL_INCLUDEDIR}${pc_fimex_includedir}"
 
 SET(FIMEX_PC "fimex${MINUS_FIMEX_VERSION}.pc")
 FIMEX_GEN_PKGCONFIG(fimex.pc.in "${FIMEX_PC}"
-  "${FIMEX_PC_REQUIRES}"
-  "${grib_api_LIB};${libxml2_LIB};${netcdf_LIB};${proj_LIB};${udunits2_LIB};${eccodes_LIB}"
+  ""
+  "" 
   "${CMAKE_INSTALL_LIBDIR}"
   "${FIMEX_INSTALL_INCLUDEDIR};${udunits2_INC_DIR};${proj_INC_DIR};${grib_api_INC_DIR}"
 )

src/CMakeLists.txt
@@ -1,20 +1,5 @@
 
-SET(libfimex_INCLUDE_DIRECTORIES
-  ${PC_INCLUDE_DIRS}
-  ${grib_api_INC_DIR}
-  ${libxml2_INC_DIR}
-  ${netcdf_INC_DIR}
-  ${proj_INC_DIR}
-  ${udunits2_INC_DIR}
-  ${ECCODES_INCLUDE_DIRS}
-  ${date_INC_DIR}
-)
-
-SET(libfimex_COMPILE_OPTIONS
-  ${PC_CFLAGS_OTHER}
-  )
 SET(FIMEX_DATADIR "${CMAKE_INSTALL_FULL_DATADIR}/fimex${MINUS_FIMEX_VERSION}")
-LINK_DIRECTORIES(${PC_LIBRARY_DIRS})
 
 SET(INCF "${CMAKE_SOURCE_DIR}/include/fimex")
 
@@ -301,7 +286,6 @@ ENDIF(ENABLE_NETCDF)
 IF((ENABLE_GRIBAPI) OR (ENABLE_ECCODES))
   IF(ENABLE_GRIBAPI)
     SET(HAVE_GRIB_API 1)
-    LIST(APPEND libfimex_INCLUDE_DIRECTORIES ${GRIBAPI_INC_DIR})
   ELSE()
     SET(HAVE_ECCODES 1)
   ENDIF()
@@ -425,18 +409,20 @@ SET(libfimex_ALL_SOURCES
   ${libfimex_proradxml_SOURCES}
 )
 
-SET(libfimex_LIBS
+SET(libfimex_PACKAGES
   ${FELT_LIBRARY}
-  ${PC_LIBRARIES}
-  ${grib_api_LIB}
-  ${libxml2_LIB}
-  ${netcdf_LIB}
-  ${proj_LIB}
-  ${udunits2_LIB}
-  ${eccodes_LIB}
-  ${date_LIB}
+  ${date_PACKAGE}
+  ${eccodes_PACKAGE}
+  ${grib_api_PACKAGE}
+  ${libxml2_PACKAGE}
+  ${log4cpp_PACKAGE}
+  ${metgm_PACKAGE}
+  ${netCDF_PACKAGE}
+  ${proj_PACKAGE}
+  ${proradxmlrw_PACKAGE}
+  ${udunits2_PACKAGE}
 )
 
-FIMEX_ADD_LIBRARY(fimex "${libfimex_ALL_SOURCES}" "${libfimex_LIBS}" "${libfimex_INCLUDE_DIRECTORIES}" "${libfimex_COMPILE_OPTIONS}")
+FIMEX_ADD_LIBRARY(fimex "${libfimex_ALL_SOURCES}" "${libfimex_PACKAGES}")
 
 ADD_SUBDIRECTORY(binSrc)

src/binSrc/CMakeLists.txt
@@ -1,34 +1,32 @@
 
-SET(LIBRARIES
+SET(PACKAGES
   libfimex
   mi-programoptions
-  ${libxml2_LIB}
+  ${libxml2_PACKAGE}
+  ${log4cpp_PACKAGE}
 )
 
-FUNCTION(ADD_EXE name_ includes_ libs_)
+FUNCTION(ADD_EXE name_ packages_)
   ADD_EXECUTABLE("${name_}" "${name_}.cc")
   SET_TARGET_PROPERTIES("${name_}" PROPERTIES
     OUTPUT_NAME "${name_}${MINUS_FIMEX_VERSION}"
     )
-  TARGET_LINK_LIBRARIES("${name_}" ${LIBRARIES} ${libs_})
-  TARGET_INCLUDE_DIRECTORIES("${name_}" PRIVATE ${includes_})
+  TARGET_LINK_LIBRARIES("${name_}" PRIVATE ${PACKAGES} ${packages_})
   INSTALL(TARGETS "${name_}" DESTINATION bin)
 ENDFUNCTION()
 
-LINK_DIRECTORIES(${PC_LIBRARY_DIRS})
-INCLUDE_DIRECTORIES(${PC_INCLUDE_DIRS}
+INCLUDE_DIRECTORIES(
   "${CMAKE_SOURCE_DIR}/src" # for CDMMergeUtils.h, ...
   "${CMAKE_BINARY_DIR}/src" # for fimex_config.h
 )
 
-ADD_EXE(fimex "" "${PC_LIBRARIES}")
-ADD_EXE(fiXYcontents "" "")
+ADD_EXE(fimex "")
+ADD_EXE(fiXYcontents "")
 
 IF((ENABLE_GRIBAPI) OR (ENABLE_ECCODES))
-  SET(GRIB_INCDIRS ${grib_api_INC_DIR} ${ECCODES_INCLUDE_DIRS})
-  SET(GRIB_LIBS ${grib_api_LIB} ${eccodes_LIB})
+  SET(GRIB_PACKAGES ${eccodes_PACKAGE} ${grib_api_PACKAGE})
 
-  ADD_EXE(fiIndexGribs "${GRIB_INCDIRS}" "${GRIB_LIBS}")
-  ADD_EXE(fiGribCut "${GRIB_INCDIRS}" "${GRIB_LIBS}")
-  ADD_EXE(fiGrbmlCat "" "${PC_LIBRARIES}")
+  ADD_EXE(fiIndexGribs "${GRIB_PACKAGES}")
+  ADD_EXE(fiGribCut    "${GRIB_PACKAGES}")
+  ADD_EXE(fiGrbmlCat   "")
 ENDIF()

src/felt/CMakeLists.txt
@@ -1,7 +1,4 @@
 
-LINK_DIRECTORIES(
-)
-
 LIST(APPEND libfelt_SOURCES
   FeltField.cpp
   FeltFile.cpp
@@ -9,15 +6,11 @@ LIST(APPEND libfelt_SOURCES
   FeltGridDefinition.cpp
   )
 
-SET (libfelt_INCLUDE_DIRS
-  "${date_INC_DIR}"
+SET(libfelt_PACKAGES
+  ${date_PACKAGE}
+  ${proj_PACKAGE}
 )
-
-SET (libfelt_LIBS
-  ${date_LIB}
-)
-
-FIMEX_ADD_LIBRARY(felt "${libfelt_SOURCES}" "${libfelt_LIBS}" "${libfelt_INCLUDE_DIRS}" "")
+FIMEX_ADD_LIBRARY(felt "${libfelt_SOURCES}" "${libfelt_PACKAGES}")
 
 IF(TARGET libfelt)
   TARGET_INCLUDE_DIRECTORIES(libfelt PRIVATE "${CMAKE_BINARY_DIR}/src")

test/CMakeLists.txt
@@ -1,5 +1,5 @@
 
-LINK_DIRECTORIES(${PC_LIBRARY_DIRS}
+LINK_DIRECTORIES(
   ${BOOST_LIBRARY_DIRS} # if using boost unit_test_framework
 )
 
@@ -144,7 +144,7 @@ ENDIF(ENABLE_METGM)
 IF (ENABLE_NETCDF)
   FIND_PROGRAM(NCDUMP_PROGRAM
     NAMES ncdump
-    HINTS "${PC_netcdf_PREFIX}/bin" "${netcdf_DIR}/bin"
+    HINTS "${netcdf_DIR}/bin"
   )
   IF (NCDUMP_PROGRAM)
     MESSAGE (STATUS "Found ncdump: ${NCDUMP_PROGRAM}")
@@ -186,10 +186,16 @@ ENDIF()
 
 FOREACH(T ${CC_TESTS})
   ADD_EXECUTABLE(${T} "${T}.cc")
-  TARGET_INCLUDE_DIRECTORIES(${T} PRIVATE ${Boost_INCLUDE_DIRS} ${PC_INCLUDE_DIRS})
-  TARGET_LINK_LIBRARIES(${T} libfimex testinghelpers ${Boost_LIBRARIES} ${PC_LIBRARIES})
+  TARGET_INCLUDE_DIRECTORIES(${T} PRIVATE ${Boost_INCLUDE_DIRS})
+  TARGET_LINK_LIBRARIES(${T}
+    libfimex
+    testinghelpers
+    ${Boost_LIBRARIES}
+  )
   ADD_TEST(NAME ${T} COMMAND ${T})
 ENDFOREACH()
+
+TARGET_LINK_LIBRARIES(testXMLDoc ${libxml2_PACKAGE})
 
 FOREACH(T ${C_TESTS})
   ADD_EXECUTABLE(${T} "${T}.c")

commit ea194ac5e3dfe1de5e1e5e058ff04dacb6eddecf

bugfix FIMEX_ADD_LIBRARY
 * add packages for static lib
 * regression in commit ef930a19fc

cmake/FimexUtils.cmake
@@ -181,7 +181,7 @@ FUNCTION(FIMEX_ADD_LIBRARY name sources packages)
     SET(static_lib lib${name}-static)
     SET(lib${name}_STATICLIBS ${libs})
     ADD_LIBRARY(${static_lib} STATIC ${sources})
-    TARGET_LINK_LIBRARIES(${static_lib} PRIVATE ${libs})
+    TARGET_LINK_LIBRARIES(${static_lib} PRIVATE ${packages})
     TARGET_INCLUDE_DIRECTORIES(${static_lib}
       PUBLIC
       $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/include>