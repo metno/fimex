635b580469f28d7cfc11eee32fd72224e6ee0346
Alexander Bürger <alexander.buerger@met.no>
2024-02-14 08:00:08 +0100

    fimex 1.10.0-1

--- include/fimex/CDMconstants.h
+++ include/fimex/CDMconstants.h
@@ -54,8 +54,8 @@
  * touch configure.ac after changing these numbers
  */
 #define MIFI_VERSION_MAJOR  1
-#define MIFI_VERSION_MINOR  9
-#define MIFI_VERSION_PATCH  9
+#define MIFI_VERSION_MINOR  10
+#define MIFI_VERSION_PATCH  0
 #define MIFI_VERSION_STATUS 0xF0
 
 #define MIFI_VERSION_PATCH_STRING "." MIFI_STRINGIFY(MIFI_VERSION_PATCH)
