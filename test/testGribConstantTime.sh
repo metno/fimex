cb4be73fe2cc08219831375b3ba5543a2188c3dd
Alexander Bürger <alexander.buerger@met.no>
2026-06-02 08:00:08 +0200

    adapt testGribConstantTime for read-only source directory

--- test/testGribConstantTime.sh
+++ test/testGribConstantTime.sh
@@ -7,12 +7,13 @@ TESTCASE="grib reader constant time"
 echo "testing $TESTCASE"
 
 TEST_GCT="${TEST_SRCDIR}/grib/constantTime"
+OUTPUTFILE="testGribConstantTime.nc"
 
 if ./fimex.sh \
        --input.file "$TEST_GCT/input.grib2" \
        --input.type grib \
        --input.config "$TEST_GCT/config.xml" \
-       --output.file "$TEST_GCT/output.nc" \
+       --output.file "$OUTPUTFILE" \
        --output.type nc4 ; then
     echo "success"
     E=0
@@ -20,5 +21,5 @@ else
     echo "failed $TESTCASE"
     E=1
 fi
-rm -vf "$TEST_GCT/output.nc"
+rm -vf "$OUTPUTFILE"
 exit $E
